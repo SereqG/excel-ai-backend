ec0c8e823741886511500b583357399177fd2f46
SereqG <serekgrom@gmail.com>
2026-01-18 18:33:34 +0100

    settings and packages update

--- excelAi/excelAi/settings.py
+++ excelAi/excelAi/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'excelAi.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 60,  # persistent connections
+        "OPTIONS": {
+            "sslmode": "require",  # production only
+        },
     }
 }
 
